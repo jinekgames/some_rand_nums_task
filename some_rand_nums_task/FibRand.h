b9f45be915ea5854e3bdb7abec547b435268ea48
jnk <ujinval@yandex.ru>
2021-05-01 19:18:00 +0300

    Add project files.

---
+++ some_rand_nums_task/FibRand.h
@@ -0,0 +1,123 @@
+﻿//////////////////////////////////////////////////////////////////////
+//																	//
+// FibRand.h														//
+//																	//
+// Генератора случайных чисел (вариант 10 (на числах Фибоначчи)		//
+//																	//
+//////////////////////////////////////////////////////////////////////
+
+
+#pragma once
+
+#include <list>
+#include <vector>
+
+
+// коэффициенты
+
+int A = 1;
+int B = 2;
+int C = 3;
+unsigned int K = 5u;		// также определяет размер буфера (дб целочисленным)
+unsigned int M = 30u;
+
+
+//тип данных вычисляемых значений
+typedef int TYPE;
+
+// буфер с вычисленными ранее значениями
+std::list<TYPE> v(K);
+
+// функция подсчета нового элемента
+TYPE calculate() {
+
+	// вычисляемый элемент
+	TYPE newValue;
+
+	// x(n-1)
+	auto prevElemTag = v.end();
+	prevElemTag--;
+	TYPE prevElem = *prevElemTag;
+	// x(n-k)
+	TYPE lastElem = *(v.begin());
+
+
+	// собственно вычисление
+	newValue = (A * prevElem + B * lastElem + C) % M;
+
+
+	// удаление первого элемента буфера
+	v.pop_front();
+	// добавление вычисленного значения в буфер
+	v.push_back(newValue);
+
+	// возврат вычисленного значения
+	return newValue;
+
+}
+
+// инициализация модуля
+void init(int a, int b, int c, unsigned int k, unsigned int m) {
+
+	A = a;
+	B = b;
+	C = c;
+	K = k;
+	M = m;
+
+	//изменение длины буфера
+	//
+
+}
+
+// задание начальных значений
+void seed(std::vector<TYPE> startValues) {
+
+	auto indexSeed = startValues.begin();
+
+	for (auto& indexList : v) {
+
+		indexList = *(indexSeed++);
+
+	}
+
+}
+// задание начальных значений
+void seed(std::list<TYPE> startValues) {
+
+	auto indexSeed = startValues.begin();
+
+	for (auto& indexList : v) {
+
+		indexList = *(indexSeed++);
+
+	}
+
+}
+// задание начальных значений
+void seed(TYPE n, ...) {
+
+	TYPE* ptr = &n;
+	unsigned int index = 1;
+
+	for (auto& indexList : v) {
+
+		indexList = ptr[index];
+
+		index++;
+		if (index > n) {
+			return;
+		}
+
+	}
+
+}
+
+
+
+// получение случайного числа
+TYPE getRand() {
+
+	return calculate();
+
+}
