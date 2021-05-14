b23ce8c2528a5a7a0fda5be973f1a5728f832837
jnk <ujinval@yandex.ru>
2021-05-15 00:41:03 +0300

    вот тут я начал фигню именно артема делать, но не закончил, че за лаги лол, у меня не лагает :/

--- some_rand_nums_task/main.cpp
+++ some_rand_nums_task/main.cpp
@@ -10,58 +10,45 @@
 #include <iostream>
 #include <fstream>
 #include <Windows.h>
-
+#include <math.h>
 #include "FibRand.h"
 
 // максимальное значение для генерируемых чисел
 #define GEN_MOD_DEF 100u
 // число генерируемых значений
 #define COUNT_RND_NUMS_DEF 100u
-// число интервалов для исследования
-#define COUNT_INTERVALS_DEF 20u
-// длина каждого интервала
-#define INTERVAL_LENTH_DEF GEN_MOD_DEF / COUNT_INTERVALS_DEF
+
+#define A 10
+#define B 2
+#define C 3
 
 
 int main() {
 
 	srand(time(NULL));
 
-	// массив сгененрированных N чисел
-	std::vector<int> rndValues(COUNT_RND_NUMS_DEF);
-	// сюда мы запишем сколько чисел попало в каждый из интервалов
-	static unsigned int numsInIntrvls[COUNT_INTERVALS_DEF];
-	// эксперементальные вероятности
-	double expProbs[COUNT_INTERVALS_DEF];
+	// массив X сгененрированных чисел
+	std::vector<int> Xrnd(COUNT_RND_NUMS_DEF);
+	// массив X сгененрированных чисел
+	std::vector<int> Yrnd(COUNT_RND_NUMS_DEF);
 
-	init(10, 2, 3, 5u, GEN_MOD_DEF);
+	init(A, B, C, 5u, GEN_MOD_DEF);
 	seed(5, rand(), rand(), rand(), rand(), rand());
 
 	// генерация чисел (и сохранение в массив)
-	for (auto& index : rndValues) {
+	for (auto& index : Xrnd) {
 		index = getRand();
 	}
 
-	// подсчет чисел, попавших в заданные интервалы
-	for (auto index : rndValues) {
-		for (auto i = 0; i < COUNT_INTERVALS_DEF; i++) {
-
-			index -= INTERVAL_LENTH_DEF;
-			if (index < 0) {
-				numsInIntrvls[i]++;
-				break;
-			}
-
-		}
+	// вычисление второго массива
+	for (auto i = 0; i < COUNT_RND_NUMS_DEF; i++) {
+		Yrnd[i] = A * sin(Xrnd[i]) * sin(Xrnd[i]) + B * sin(Xrnd[i]) + C;
 	}
 
-	// открытие файла для записи чисел тобы потом график сделать красиво (вау)
-	std::ofstream fout("results.txt", std::ios::trunc);
 
-	// вывод эксперементальных вероятностей в файл
-	for (auto i = 0; i < COUNT_INTERVALS_DEF; i++) {
-		expProbs[i] = double(numsInIntrvls[i]) / COUNT_RND_NUMS_DEF;
-		fout << expProbs[i] << "\n";
+	// хз че терь с этим делать, вывести на экран могу :/
+	for (auto i = 0; i < COUNT_RND_NUMS_DEF; i++) {
+		std::cout << Xrnd[i] << "\t" << Yrnd[i] << std::endl;
 	}
 
 
